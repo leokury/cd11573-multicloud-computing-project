399a672f1e3264fff8804dad51e6e82730150d52
leokury <leokury@gmail.com>
2023-03-08 11:01:54 -0300

    first revision

--- starter/azure/main.tf
+++ starter/azure/main.tf
@@ -1,5 +1,5 @@
 data "azurerm_resource_group" "udacity" {
-  name     = "Regroup_3bRvUozGAtf"
+  name     = "Regroup_4aQq0G7aKDSf"
 }
 
 resource "azurerm_container_group" "udacity" {
@@ -52,5 +52,38 @@ resource "azurerm_sql_database" "example" {
 
   tags = {
     environment = "production"
+  }
+}
+
+resource "azurerm_app_service_plan" "example" {
+  name                = "example-appserviceplan"
+  location            = data.azurerm_resource_group.udacity.location
+  resource_group_name = data.azurerm_resource_group.udacity.name
+
+  sku {
+    tier = "Standard"
+    size = "S1"
+  }
+}
+
+resource "azurerm_app_service" "example" {
+  name                = "leokury-udacity-app-service"
+  location            = data.azurerm_resource_group.udacity.location
+  resource_group_name = data.azurerm_resource_group.udacity.name
+  app_service_plan_id = azurerm_app_service_plan.example.id
+
+  site_config {
+    dotnet_framework_version = "v4.0"
+    scm_type                 = "LocalGit"
+  }
+
+  app_settings = {
+    "SOME_KEY" = "my-app-key"
+  }
+
+  connection_string {
+    name  = "Database"
+    type  = "SQLAzure"
+    value = "Data Source=tcp:${azurerm_sql_server.example.fully_qualified_domain_name},1433;User Id=${azurerm_sql_server.example.administrator_login};Password='${azurerm_sql_server.example.administrator_login_password}';"
   }
 }
